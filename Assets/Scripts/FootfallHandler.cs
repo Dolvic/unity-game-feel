536c7745c27db256e114aff208f5595007a6a4a6
Gupta, Akshay <akshay_gupta@gatech.edu>
2016-09-25 16:43:05 -0400

    Generate footstep sounds using animation events

---
+++ Assets/Scripts/FootfallHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootfallHandler : MonoBehaviour
+{
+    public AudioSource sound;
+
+    public void OnFootfall()
+    {
+        if (sound != null)
+        {
+            sound.Play();
+        }
+    }
+}
