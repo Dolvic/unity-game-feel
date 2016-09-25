66dff4cf6a403cfb4a033f39560c32b80e71ca96
Gupta, Akshay <akshay_gupta@gatech.edu>
2016-09-25 18:41:12 -0400

    Add basic particle effects to footfalls

--- Assets/Scripts/FootfallHandler.cs
+++ Assets/Scripts/FootfallHandler.cs
@@ -1,15 +1,33 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
 public class FootfallHandler : MonoBehaviour
 {
     public AudioSource sound;
+    public ParticleSystem leftParticles;
+    public ParticleSystem rightParticles;
 
-    public void OnFootfall()
+    public void OnLeftFootfall()
+    {
+        SimulateFootfall(leftParticles);
+    }
+
+    public void OnRightFootfall()
+    {
+        SimulateFootfall(rightParticles);
+    }
+
+    private void SimulateFootfall(ParticleSystem particles)
     {
         if (sound != null)
         {
             sound.Play();
+        }
+
+        if (particles != null)
+        {
+            particles.Play();
         }
     }
 }
