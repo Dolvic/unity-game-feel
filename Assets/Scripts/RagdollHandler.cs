56c504415b6020068d8d45e997a50d24f542a66e
Gupta, Akshay <akshay_gupta@gatech.edu>
2016-09-26 17:01:56 -0400

    Add a handler for handling ragdoll

---
+++ Assets/Scripts/RagdollHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RagdollHandler : MonoBehaviour
+{
+    public GameObject character;
+    public GameObject ragdollPrefab;
+
+    private  GameObject ragdoll;
+
+    public void Update()
+    {
+        if (!Input.GetKeyUp(KeyCode.Space))
+        {
+            return;
+        }
+
+        var characterActive = character.activeInHierarchy;
+        character.SetActive(!characterActive);
+
+        if (characterActive)
+        {
+            ragdoll =
+                Instantiate(ragdollPrefab, character.transform.position, character.transform.rotation) as GameObject;
+        }
+        else
+        {
+            Destroy(ragdoll);
+        }
+    }
+}
