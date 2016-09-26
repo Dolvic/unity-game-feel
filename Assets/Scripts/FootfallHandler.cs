529af5d8fbcb6c29831cdb96d6b72df82cb43af7
Gupta, Akshay <akshay_gupta@gatech.edu>
2016-09-26 16:10:51 -0400

    Save progress

--- Assets/Scripts/FootfallHandler.cs
+++ Assets/Scripts/FootfallHandler.cs
@@ -1,31 +1,64 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class FootfallHandler : MonoBehaviour
 {
-    public AudioSource sound;
+    public AudioSource woodSound;
+    public AudioSource metalSound;
     public ParticleSystem leftParticles;
     public ParticleSystem rightParticles;
 
+    private Rigidbody rigidBody;
+    private bool onMetal;
+    private bool onGround;
+
+    public void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate()
+    {
+        var characterPosition = rigidBody.position;
+        onGround = Math.Abs(characterPosition.y) < .001;
+        var position = characterPosition;
+        position.y += 1;
+        RaycastHit hit;
+        var wasHit = Physics.Raycast(position, Vector3.down, out hit, position.y, 1 << 8);
+        if (wasHit)
+        {
+            onMetal = hit.collider.CompareTag("MetalFloor");
+            onGround = onGround || onMetal;
+        }
+    }
+
     public void OnLeftFootfall()
     {
-        SimulateFootfall(leftParticles);
+        if (onGround)
+        {
+            SimulateFootfall(leftParticles);
+        }
     }
 
     public void OnRightFootfall()
     {
-        SimulateFootfall(rightParticles);
+        if (onGround)
+        {
+            SimulateFootfall(rightParticles);
+        }
     }
 
     private void SimulateFootfall(ParticleSystem particles)
     {
+        var sound = onMetal ? metalSound : woodSound;
         if (sound != null)
         {
             sound.Play();
         }
 
-        if (particles != null)
+        if (!onMetal && particles != null)
         {
             particles.Play();
         }
